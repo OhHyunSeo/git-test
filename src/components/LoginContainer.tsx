6a48776863910858a170fa73497883089ec4f2d0
mintaek <mintaek970708@gmail.com>
2024-02-02 15:31:03 +0900

    Project data init

---
+++ src/components/LoginContainer.tsx
@@ -0,0 +1,16 @@
+'use client'
+import React, { useState } from 'react';
+
+export default function LoginContainer() {
+    const [loginForm, setLoginForm] = useState({
+        id: '',
+        pw: '',
+    })
+
+    return (
+        <div className='w-[360px] h-[340px] absolute left-[5%] top-[50%] bg-[#2d1010] rounded-md'>
+            
+        </div>
+    );
+}
+
