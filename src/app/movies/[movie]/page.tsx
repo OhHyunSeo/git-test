269b522ebc35acbd8d5ebb8f54a3dfac371ee512
Mintaek_CHO <mintaek5555@gmail.com>
2024-02-07 20:50:38 +0900

    styles: 마이페이지 퍼블리싱 완료

--- src/app/movies/[movie]/page.tsx
+++ src/app/movies/[movie]/page.tsx
@@ -1,99 +1,88 @@
-"use client";
-import Divider from "@/components/common/Divider";
-import MovieInfo from "@/components/movie/MovieInfo";
-import PlaceBox from "@/components/movie/PlaceBox";
-import ReviewContainer from "@/components/movie/ReviewContainer";
-import { MovieDataType, MoviePlaceDataType } from "@/type/movieType";
-import axios from "axios";
-import React, { Fragment, useEffect, useState } from "react";
+'use client';
+import Divider from '@/components/common/Divider';
+import MovieInfo from '@/components/movie/MovieInfo';
+import PlaceBox from '@/components/movie/PlaceBox';
+import ReviewContainer from '@/components/movie/ReviewContainer';
+import { MovieDataType, MoviePlaceDataType } from '@/type/movieType';
+import axios from 'axios';
+import React, { Fragment, useEffect, useState } from 'react';
 
-export default function MovieDetailPage({
-  params,
-}: {
-  params: { movie: string | number };
-}) {
-  const CATEGORY_LIST = ["촬영지 선택", "리뷰 보기"];
-  const [currentPage, setCurrentPage] = useState(0);
-  const [moviePlaceData, setMoviePlaceData] = useState<MoviePlaceDataType[]>([]);
-  const [movieInfoData, setMovieInfoData] = useState<MovieDataType>();
-  const [selectedPlace, setSelectedPlace] = useState<number[]>([]);
+export default function MovieDetailPage({ params }: { params: { movie: string | number } }) {
+    const CATEGORY_LIST = ['촬영지 선택', '리뷰 보기'];
+    const [currentPage, setCurrentPage] = useState(0);
+    const [moviePlaceData, setMoviePlaceData] = useState<MoviePlaceDataType[]>([]);
+    const [movieInfoData, setMovieInfoData] = useState<MovieDataType>();
+    const [selectedPlace, setSelectedPlace] = useState<number[]>([]);
 
-  useEffect(() => {
-    axios
-      .get(`${process.env.NEXT_PUBLIC_LOCAL_URL}/api/movie/${params.movie as string}`, {
-        params: { title: decodeURIComponent(params.movie as string) },
-      })
-      .then((res) => {
-        console.log(res)
-        setMoviePlaceData(res.data.findMoviePlace);
-        setMovieInfoData(res.data.findMovie[0]);
-      });
-  }, []);
+    useEffect(() => {
+        axios
+            .get(`${process.env.NEXT_PUBLIC_LOCAL_URL}/api/movie/${params.movie as string}`, {
+                params: { title: decodeURIComponent(params.movie as string) },
+            })
+            .then((res) => {
+                console.log(res);
+                setMoviePlaceData(res.data.findMoviePlace);
+                setMovieInfoData(res.data.findMovie[0]);
+            });
+    }, []);
 
-  const handleClick = (id: number) => {
-    if (selectedPlace.includes(id)) {
-      const filter = selectedPlace.filter((item) => item !== id);
-      setSelectedPlace(filter);
-    } else {
-      setSelectedPlace((prev) => [...prev, id]);
-    }
-  };
+    const handleClick = (id: number) => {
+        if (selectedPlace.includes(id)) {
+            const filter = selectedPlace.filter((item) => item !== id);
+            setSelectedPlace(filter);
+        } else {
+            setSelectedPlace((prev) => [...prev, id]);
+        }
+    };
 
-  const handleSubmit = () => {};
-  console.log(moviePlaceData)
+    const handleSubmit = () => {};
+    console.log(moviePlaceData);
 
-  return movieInfoData && (
-    <div className="w-full h-full min-w-[1920px]  flex flex-col items-center">
-      <Divider />
-      <div className="w-full flex">
-        {CATEGORY_LIST.map((category, i) => (
-          <div
-            key={i}
-            className={`flex flex-1 h-[50px] items-center justify-center gap-6 font-[600] cursor-pointer ${
-              i !== CATEGORY_LIST.length - 1 ? "border-r border-[#c1c1c1]" : ""
-            }`}
-            onClick={() => setCurrentPage(i)}
-          >
-            <p>{category}</p>
-            {currentPage === i && (
-              <div className="w-3 h-3 rounded-full bg-[#3164f4]" />
-            )}
-          </div>
-        ))}
-      </div>
-      {CATEGORY_LIST[currentPage] === "촬영지 선택"  ? (
-        <>
-          <MovieInfo
-            movieInfo={movieInfoData}
-            handleSubmit={handleSubmit}
-          />
+    return (
+        movieInfoData && (
+            <div className="w-full h-full min-w-[1920px]  flex flex-col items-center">
+                <Divider />
+                <div className="w-full flex">
+                    {CATEGORY_LIST.map((category, i) => (
+                        <div
+                            key={i}
+                            className={`flex flex-1 h-[50px] items-center justify-center gap-6 font-[600] cursor-pointer ${
+                                i !== CATEGORY_LIST.length - 1 ? 'border-r border-[#c1c1c1]' : ''
+                            }`}
+                            onClick={() => setCurrentPage(i)}
+                        >
+                            <p>{category}</p>
+                            {currentPage === i && <div className="w-3 h-3 rounded-full bg-[#3164f4]" />}
+                        </div>
+                    ))}
+                </div>
+                {CATEGORY_LIST[currentPage] === '촬영지 선택' ? (
+                    <>
+                        <MovieInfo movieInfo={movieInfoData} handleSubmit={handleSubmit} />
 
-          <div className="w-full relative">
-            <Divider />
-            <div className="absolute left-1/2 -translate-x-1/2 -top-8">
-              <p className="text-[#030303] text-[24px] font-[700]">
-                영화의 촬영지를 골라주세요
-              </p>
+                        <div className="w-full relative">
+                            <Divider />
+                            <div className="absolute left-1/2 -translate-x-1/2 -top-8">
+                                <p className="text-[#030303] text-[24px] font-[700]">영화의 촬영지를 골라주세요</p>
+                            </div>
+                        </div>
+                        <div className="w-full max-w-[1920px] grid grid-cols-5 gap-y-4 px-16 py-8">
+                            {moviePlaceData?.map((movie) => (
+                                <PlaceBox
+                                    key={movie.moviePlaceId}
+                                    movie={movie}
+                                    handleClick={handleClick}
+                                    selectedPlace={selectedPlace}
+                                />
+                            ))}
+                        </div>
+                    </>
+                ) : (
+                    <>
+                        <ReviewContainer movieInfo={movieInfoData} />
+                    </>
+                )}
             </div>
-          </div>
-          <div className="w-full max-w-[1920px] grid grid-cols-5 gap-y-4 px-16 py-8">
-            {moviePlaceData?.map((movie) => (
-              <PlaceBox
-                key={movie.moviePlaceId}
-                movie={movie}
-                handleClick={handleClick}
-                selectedPlace={selectedPlace}
-              />
-            ))}
-          </div>
-        </>
-      ) : (
-        <>
-          <ReviewContainer
-            movieInfo={movieInfoData}
-          />
-        </>
-      )}
-    </div>
-  );
+        )
+    );
 }
